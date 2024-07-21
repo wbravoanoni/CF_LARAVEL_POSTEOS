6c6f791eb2ea00a0e6b1279836a0e6d21362b082
=Winston bravo <wbravoanoni@gmail.com>
2024-07-21 00:00:17 -0400

    solo ver post de mis amigos

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -8,11 +8,16 @@ use App\Models\Post;
 class PageController extends Controller
 {
     public function dashboard(Request $request){
-        dd($request->user()->friendsFrom()->get(),$request->user()->friendsTo()->get());
-        
+
         if( $request->get('for-my') ){
-            //$posts = Post::where('user_id', $request->user()->id)->get(); 
-            $posts = $request->user()->posts()->latest()->get();
+            $user = $request->user();
+
+            $friends_from_ids = $request->user()->friendsFrom()->pluck('users.id');
+            $friends_to_ids = $request->user()->friendsTo()->pluck('users.id');
+            $user_ids = $friends_from_ids->merge($friends_to_ids)->push($user->id);
+            
+            $posts = Post::whereIn('user_id',$user_ids)->latest()->get();;
+
         }else{
             $posts = Post::latest()->get(); 
         }
